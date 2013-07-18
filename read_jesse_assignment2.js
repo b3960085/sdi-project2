7f83300bbf8c7c39670203be766b1ec984689803
Jesse Read <darthnerdus@me.com>
2013-07-18 15:35:14 -0400

    Added initial variable declarations from previous project and outlined new variables with configurations.

--- read_jesse_assignment2.js
+++ read_jesse_assignment2.js
@@ -1,4 +1,15 @@
 /*  Jesse Read
     Project 2
     07/18/13
-    Theme: A music-playing application for a mobile device. */
+    Theme: A music-playing application for a mobile device. */    Theme: A music-playing application for a mobile device. */
+
+// Variable Declaration
+var firstRun = true;
+// firstRun = false; // Uncomment if "previously run" workflow is desired.
+var selectedArtist = "Daft Punk";
+var selectedAlbum = "Random Access Memories";
+var daftPunkAlbums = ["Homework", "Discovery", "Daft Club", "Human After All", "TRON: Legacy", "TRON: Legacy R3CONF1GUR3D", "Random Access Memories"];
+var allMusicAvailable;
+var artistCount = 526;
+// artistCount = 234; // Uncomment if "fully synced" workflow is desired. Requires firstRun = false
+var availableArtists;
