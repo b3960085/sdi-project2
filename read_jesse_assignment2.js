cfcb2b9e7a481477c573fd2cc2f54829277648a0
Jesse Read <darthnerdus@me.com>
2013-07-18 15:39:00 -0400

    Added first function.

--- read_jesse_assignment2.js
+++ read_jesse_assignment2.js
@@ -13,3 +13,17 @@ var allMusicAvailable;
 var artistCount = 526;
 // artistCount = 234; // Uncomment if "fully synced" workflow is desired. Requires firstRun = false
 var availableArtists;
+
+// Function Declarations
+// Procedure
+var welcomeMessage = function(firstLaunch) {
+    if (firstLaunch) {
+        // True, first time the application has been launched. Display welcome.
+        console.log("Welcome to your new streaming music app! Since this is your first launch, the application will now sync artist and album information from your server.");
+        availableArtists = 0;
+    } else {
+        // False, welcome user back and display collection size.
+        console.log("Welcome back! You currenty have 234 artists available. Enjoy!");
+        availableArtists = 234;
+    }
+}
